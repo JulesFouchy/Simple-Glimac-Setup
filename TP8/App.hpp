e5c5fdb4029328a0124dc035a123106eeb276adb
Jules Fouchy <jules.fouchy@ntymail.com>
2021-09-25 13:39:06 +0200

    Setup 9 TPs

---
+++ TP8/App.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+class App {
+public:
+    App(int window_width, int window_height);
+    void render();
+    void key_callback(int key, int scancode, int action, int mods);
+    void mouse_button_callback(int button, int action, int mods);
+    void scroll_callback(double xoffset, double yoffset);
+    void cursor_position_callback(double xpos, double ypos);
+    void size_callback(int width, int height);
+
+private:
+    int _width{};
+    int _height{};
+};
---
+++ TP8/main.cpp
@@ -0,0 +1,62 @@
+#include <GLFW/glfw3.h>
+#include "App.hpp"
+
+static App& get_app(GLFWwindow* window)
+{
+    return *reinterpret_cast<App*>(glfwGetWindowUserPointer(window));
+}
+
+int main()
+{
+    /* Initialize the library */
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    /* Create a windowed mode window and its OpenGL context */
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Hello World", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Create the App */
+    int w, h;
+    glfwGetWindowSize(window, &w, &h);
+    App app{w, h};
+
+    /* Hook user inputs to the App */
+    glfwSetWindowUserPointer(window, reinterpret_cast<void*>(&app));
+    glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
+        get_app(window).key_callback(key, scancode, action, mods);
+    });
+    glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) {
+        get_app(window).mouse_button_callback(button, action, mods);
+    });
+    glfwSetScrollCallback(window, [](GLFWwindow* window, double xoffset, double yoffset) {
+        get_app(window).scroll_callback(xoffset, yoffset);
+    });
+    glfwSetCursorPosCallback(window, [](GLFWwindow* window, double xpos, double ypos) {
+        get_app(window).cursor_position_callback(xpos, ypos);
+    });
+    glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+        get_app(window).size_callback(width, height);
+    });
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        app.render();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
